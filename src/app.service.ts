e8f724e5d2074e34332249a4a678d95f339c9f5c
Gian Lazzarini <gianlazzarini@gmail.com>
2024-05-20 19:29:54 -0700

    wip

--- src/app.service.ts
+++ src/app.service.ts
@@ -25,27 +25,30 @@ export class AppService {
     console.log(ffmpegPath)
     this.ffmpegMicOutput = this.startFfmpegMicProcess();
     this.ffmpegMicOutput.on('data', (chunk) => {
-      // if (this.broadcastQueue.length) {
-      //   // could merge buffers here
-      //   console.log('dequeue broadcast buffer');
-      //   this.stream.push(this.broadcastQueue.shift());
-      // } else {
-      //   this.stream.push(chunk);
-      // }
       this.stream.push(chunk);
     });
+    this.stream.on('data', (chunk) => console.log('stream on data', chunk.length));
+    this.stream.on('close', () => console.log('stream close'));
+    this.stream.on('end', () => console.log('stream end'));
+    this.stream.on('drain', () => console.log('stream drain'));
+    this.stream.on('error', (err) => console.error('stream err', err));
+    this.stream.on('finish', () => console.log('stream finish'));
+    this.stream.on('pause', () => console.log('stream pause'));
+    this.stream.on('pipe', () => console.log('stream pipe'));
+    this.stream.on('readable', () => console.log('stream readable'));
+    this.stream.on('resume', () => console.log('stream resume'));
+    this.stream.on('unpipe', () => console.log('stream unpipe'));
   }
 
   broadcast(buffer: Buffer) {
-    console.log(buffer);
-    Readable.from(buffer);
-    this.broadcastQueue.push(buffer);
+    this.stream.push(buffer);
   }
 
   startFfmpegMicProcess() {
     // ffmpeg -use_wallclock_as_timestamps true -f wav -re -i pipe: -codec:a aac -b:a 128k -af aresample=async=1 -f wav -
     // ffmpeg -use_wallclock_as_timestamps true -f avfoundation -i :1 -f wav -re -i pipe: -codec:a aac -b:a 128k -af aresample=async=1 -filter_complex amerge=inputs=2 -f wav -
     this.ffmpegMicProcess = spawn(ffmpegPath, [
+      '-hide_banner',
       '-f', 'avfoundation', // mac os media devices
       '-i', ':1', // mac os microphone input
       '-f', 'wav',
