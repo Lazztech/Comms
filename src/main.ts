6cb09243e23267b76885acf3186b39e35e808e1d
Gian Lazzarini <gianlazzarini@gmail.com>
2024-05-20 21:03:09 -0700

    self signed cert

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as pem from 'pem';
 
-async function bootstrap() {
+async function bootstrap(httpsOptions: { key: any, cert: any }) {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
+    { httpsOptions }
   );
-  
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
   await app.listen(3000);
 }
-bootstrap();
+
+pem.createCertificate({ days: 1, selfSigned: true }, (err, keys) => {
+  bootstrap({ key: keys.clientKey, cert: keys.certificate });
+})
